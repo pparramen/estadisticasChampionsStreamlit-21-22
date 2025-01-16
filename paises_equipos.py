d1b6dc4876133009daeaa103908bca9ce92e2c0a
Jeritoo1233 <55315984+Jeritoo1233@users.noreply.github.com>
2025-01-16 18:43:24 +0100

    Requirements

--- paises_equipos.py
+++ paises_equipos.py
@@ -26,22 +26,27 @@ def app():
     # Crear un nuevo dataframe que contenga los equipos por cada país
     df_pais = goals_goalkeeping_country_grouped.groupby('country')['club'].unique().reset_index()
     df_pais['num_clubs'] = df_pais['club'].apply(len)
+    df_pais['country'] = df_pais['country'].replace({'England': 'United Kingdom'})
 
     # Cargar el mapa de Europa
     world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    print(world['name'].values)
     europe = world[world['continent'] == 'Europe']
+    uk = world[world['name'] == 'United Kingdom']
+    europe = pd.concat([europe, uk], ignore_index=True)
     europe = europe.merge(df_pais, left_on="name", right_on="country", how="left")
 
-    st.title('Análisis de equipos por país en la Champions League')
-
+    st.title('La Champions League en mapas')
+    st.divider()
+    
     # Dibujar el mapa de Europa con el número de equipos por país
     fig, ax = plt.subplots(1, 1, figsize=(12, 8))
-    ax.set_xlim(-30, 40)  # Límite de longitud para Europa
+    ax.set_xlim(-30, 50)  # Límite de longitud para Europa
     ax.set_ylim(35, 72)   # Límite de latitud para Europa
 
     # Definir los valores discretos y los colores correspondientes
     discrete_values = sorted(df_pais["num_clubs"].unique())  # Obtener los valores únicos de 'num_clubs'
-    colors_list = ["#dbe9f6", "#91c6f0", "#4f93d3", "#7ce6e2", "#07314a", "#0b4a6f"]  # Colores personalizados
+    colors_list = ["#64c5e8", "#acd4e3", "#ffd9d9", "#fc6156", "#800b03"]  # Colores personalizados
     cmap = ListedColormap(colors_list[:len(discrete_values)])  # Crear un colormap con los colores necesarios
     norm = BoundaryNorm(discrete_values + [max(discrete_values) + 1], cmap.N)  # Normalizar los valores discretos
 
@@ -55,7 +60,7 @@ def app():
         legend=True,
         legend_kwds={
             'label': "Número de equipos en Champions por país",
-            'orientation': "horizontal",
+            'orientation': "vertical",
             'shrink': 0.6,  # Ajustar tamaño de la barra
             'ticks': discrete_values  # Ajustar ticks para los valores discretos
         }
@@ -72,28 +77,60 @@ def app():
 
     # Mostrar el mapa en Streamlit
     st.image(buffer, caption="Mapa de Europa con Número de Equipos en Champions", use_container_width=True)
-
-
-    st.subheader("Gráfica de Diferencia de Goles por Club")
-
-    pais_seleccionado = st.selectbox("Selecciona un país", goals_goalkeeping_country_grouped["country"].unique())
-
+     
+    st.divider()
+    st.markdown(
+    "<h3 style='text-align: center;'>Espectadores por país</h3>",
+    unsafe_allow_html=True
+    )
+    st.image("baseDatos/EspectadoresFutbol.webp", use_container_width= 40)
+    st.divider()
+    opcion_sel = st.selectbox("Selecciona una estadística", ["Diferencia de goles", "Goles a favor"])
     # Calcular diferencia de goles
     goals_goalkeeping_country_grouped['goal_difference'] = goals_goalkeeping_country_grouped['goals'] - goals_goalkeeping_country_grouped['conceded']
     # Gráfico de diferencia de goles
-    fig, ax = plt.subplots(figsize=(10, 6))
-    df_sorted = goals_goalkeeping_country_grouped.sort_values(by='goal_difference', ascending=False)
-    # Filtrar los datos por el país seleccionado
-    bars = sns.barplot(x = 'club', y = 'goal_difference', data = df_filtrado, dodge = False)
-    ax.axhline(0, color='black', linewidth=0.8)
+    fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+    ax.set_xlim(-30, 50)  # Límite de longitud para Europa
+    ax.set_ylim(35, 72)   # Límite de latitud para Europa
+    df_pais['diferencia_goles'] = goals_goalkeeping_country_grouped.groupby('country')['goal_difference'].mean().values
+    df_pais['goles_a_favor'] = goals_goalkeeping_country_grouped.groupby('country')['goals'].mean().values
+    df_pais['goles_en_contra'] = goals_goalkeeping_country_grouped.groupby('country')['conceded'].mean().values
+    if opcion_sel == "Diferencia de goles":
+        columna = "diferencia_goles"
+        label = "Diferencia de goles"
+    elif opcion_sel == "Goles a favor":
+        columna = "goles_a_favor"
+        label = "Goles a favor"
+    # Mapa de europa con la diferencia de goles
+    europe = europe.merge(df_pais, left_on="name", right_on="country", how="left")
+    europe.plot(
+        column=columna,
+        cmap='coolwarm',
+        linewidth=0.8,
+        ax=ax,
+        edgecolor="0.8",
+        legend=True,
+        legend_kwds={
+            'label': label,
+            'orientation': "horizontal",
+            'shrink': 0.6,
+        }
+    )
+    if opcion_sel == "Diferencia de goles":
+        ax.set_title("Diferencia de goles por país", fontsize=16)
+    else:
+        ax.set_title("Goles a favor por país", fontsize=16)
+    ax.axis("off")
+    
+    # Guardar el gráfico en un buffer temporal
+    buffer = BytesIO()
+    plt.savefig(buffer, format="png", bbox_inches="tight")
+    buffer.seek(0)
 
-    ax.set_xlabel("Club")
-    ax.set_ylabel("Diferencia de Goles")
-    ax.set_title("Diferencia de Goles por Club")
-    ax.set_xticklabels(df_filtrado['club'], rotation=90)
-
-    st.pyplot(fig)
-
+    # Mostrar el mapa en Streamlit
+    st.image(buffer, use_container_width=True)
+    
     # Botón para volver al home
     if st.button("Volver a la Página Principal"):
         st.session_state.page = "home"  
+
